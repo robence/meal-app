1b34d44a62501c98ced4981b3c127d5c538a5fc8
rozsabence <robence@gmail.com>
2020-05-02 10:28:14 +0200

    add HetiMenu

--- src/components/HaviMenu.js
+++ src/components/HaviMenu.js
@@ -1,40 +1,15 @@
 import React from 'react';
 
-import  Table  from 'react-bootstrap/Table';
+import Container from 'react-bootstrap/Container';
+
+import HetiMenu from './HetiMenu';
 
 export default function HaviMenu() {
   return (
-    <div>
-      <p>Havi Menu</p>
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Username</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td colSpan="2">Larry the Bird</td>
-            <td>@twitter</td>
-          </tr>
-        </tbody>
-      </Table>
-    </div>
+    <Container>
+      <h2>Havi Menu</h2>
+      <HetiMenu index={1} />
+      <HetiMenu index={2} />
+    </Container>
   );
 }
---
+++ src/components/HetiMenu.js
@@ -0,0 +1,73 @@
+import React from 'react';
+
+import Table from 'react-bootstrap/Table';
+
+export default function HaviMenu({ index }) {
+  return (
+    <div>
+      <p>{`${index}. Heti Menu`}</p>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Hétfő</th>
+            <th>Kedd</th>
+            <th>Szerda</th>
+            <th>Csütörtök</th>
+            <th>Péntek</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Étel</td>
+            <td>#</td>
+            <td>#</td>
+            <td>#</td>
+            <td>#</td>
+            <td>#</td>
+          </tr>
+          <tr>
+            <td>Allergének</td>
+            <td>#</td>
+            <td>#</td>
+            <td>#</td>
+            <td>#</td>
+            <td>#</td>
+          </tr>
+          <tr>
+            <td>Só</td>
+            <td>#</td>
+            <td>#</td>
+            <td>#</td>
+            <td>#</td>
+            <td>#</td>
+          </tr>
+          <tr>
+            <td>Fehérje</td>
+            <td>#</td>
+            <td>#</td>
+            <td>#</td>
+            <td>#</td>
+            <td>#</td>
+          </tr>
+          <tr>
+            <td>Szénhidrát</td>
+            <td>#</td>
+            <td>#</td>
+            <td>#</td>
+            <td>#</td>
+            <td>#</td>
+          </tr>
+          <tr>
+            <td>Zsír</td>
+            <td>#</td>
+            <td>#</td>
+            <td>#</td>
+            <td>#</td>
+            <td>#</td>
+          </tr>
+        </tbody>
+      </Table>
+    </div>
+  );
+}
